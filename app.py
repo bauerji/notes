e500de14c630fa67bc48e34eba9e9b8faebff785
bauerji <baueji@gmail.com>
2023-05-12 16:13:51 +0200

    Reformat the code

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ moll = {
     "f": "4b",
     "c": "3b",
     "g": "2b",
-    "d": "1b"
+    "d": "1b",
 }
 
 dur = {
@@ -31,8 +31,9 @@ dur = {
     "As": "4b",
     "Es": "3b",
     "B": "2b",
-    "F": "1b"
+    "F": "1b",
 }
+
 
 def x():
     a = {"Dur": dur, "Moll": moll}
@@ -49,37 +50,23 @@ def x():
     return key, all_options, correct, b
 
 
-if __name__ == '__main__':
-    while True:
-        a = {"Dur": dur, "Moll": moll}
-        b = choice(list(a.keys()))
-
-        c = a[b]
-
-        key = choice(list(c.keys()))
-
-        correct = c[key]
-        wrong_options = list({value for value in c.values() if value != correct})[:3]
-        all_options = [correct] + wrong_options
-        shuffle(all_options)
-
-        x = {i: value for i, value in enumerate(all_options, start=1)}
-        print(x)
-        guess = input(f"{key} {b}: ")
-        if x[int(guess)] == correct:
-            print("correct")
-        else:
-            print(f"nope, the correct answer is: {correct}")
-
-
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def hello_world():
     key, all_options, correct, b = x()
     value_1, value_2, value_3, value_4 = all_options
-    return render_template('index.html', value_1=value_1, value_2=value_2, value_3=value_3, value_4=value_4, question=f"{key} {b}", correct=correct)
+    return render_template(
+        "index.html",
+        value_1=value_1,
+        value_2=value_2,
+        value_3=value_3,
+        value_4=value_4,
+        question=f"{key} {b}",
+        correct=correct,
+    )
+
 
 @app.route("/guess", methods=["POST"])
 def guess():
@@ -87,4 +74,6 @@ def guess():
     guess, correct = x.split(":")
     if guess == correct:
         return render_template("result.html", message="correct")
-    return render_template("result.html", message=f"Wrong - the correct response is {correct}")
+    return render_template(
+        "result.html", message=f"Wrong - the correct response is {correct}"
+    )
